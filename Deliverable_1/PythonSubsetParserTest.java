f8d92e579e3021db057eb2239b1422934f567b0f
Drew_Distler <dmd9g3@umsystem.edu>
2024-11-07 21:23:07 -0600

    made the java file for printing the parse tree

--- Deliverable_1/PythonSubsetParserTest.java
+++ Deliverable_1/PythonSubsetParserTest.java
@@ -6,48 +6,35 @@ import java.io.IOException;
 
 public class PythonSubsetParserTest {
     public static void main(String[] args) throws Exception {
-        // Check if the file path is passed as an argument
-        if (args.length == 0) {
-            System.out.println("Please provide a Python file as an argument.");
-            return;
-        }
-
-        String filePath = args[0]; // Get the file path from the argument
-        CharStream input = CharStreams.fromFileName(filePath); // Read input from the file
-
-        // Create a lexer that feeds off of input CharStream
-        deliverable1Lexer lexer = new deliverable1Lexer(input);
-
-        // Create a buffer of tokens pulled from the lexer
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-
-        // Create a parser that feeds off the tokens buffer
-        deliverable1Parser parser = new deliverable1Parser(tokens);
-
-        // Remove default error listeners
-        parser.removeErrorListeners();
-
-        // Add custom error listener
-        parser.addErrorListener(new DiagnosticErrorListener());
-
-        // Begin parsing at the start rule (replace 'start' with the correct rule name if needed)
-        ParseTree tree = parser.program(); // Replace 'start' with the appropriate rule if necessary
-
-        // Print the parse tree in LISP-style format
-        System.out.println(tree.toStringTree(parser));
-
-        // System.out.println("Parse Tree:");
-        // printParseTree(tree, 0); // Start with an indentation level of 0
+    if(args.length <= 0)
+    {
+        System.out.println("you need to include the test python file");
+        return;
     }
 
-    private static void printParseTree(ParseTree tree, int level) {
-        // Print the node with indentation
-        String indent = "  ".repeat(level);
-        System.out.println(indent + tree.getClass().getSimpleName() + ": " + tree.getText());
+    String file = args[0]; //this is the argument provided when runnning this file
+    try{
+    
+    String data = new String(File.readAllBytes(Paths.get(file))); //grabs all the data from the file
 
-        // If the node has children (not a leaf), recursively print each child
-        for (int i = 0; i < tree.getChildCount(); i++) {
-            printParseTree(tree.getChild(i), level + 1);
-        }
+    CharStream fileData = CharStreams.fromString(data); //we use a CharStream which is included in the antlr library
+
+    PythonSubsetLexer lexer = new PythonSubsetLexer(charStream); //makes the lexer from the charStream
+
+    CommonTokenStream stream = new CommonTokenStream(lexer);  //this gives us our tokens needed for creating the parser
+
+    PythonSubsetParser parser = new PythonSubsetParser(parser);
+
+    ParseTree tree = parser.program();
+
+    System.out.println("Parse Tree");
+
+    System.out.println(tree.toStringTree(parser));
+
     }
+    catch (IOException e)
+    {
+        System.err.println("error with reading input file");
+    }
+}
 }
